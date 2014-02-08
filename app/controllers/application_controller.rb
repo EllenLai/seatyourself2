be6483420ed5d7ca050a27b3958a93ef59dff252
EllenLai <miss.ellenlai@gmail.com>
2014-02-08 15:03:49 -0500

    showing log in and log out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_guest
+  	@current_guest ||= Guest.find(session[:guest_id]) if session[:guest_id]
+  end
+
+  helper_method :current_guest
 end
