55655708842867617f9df4645c1ffb42a81ee27c
EllenLai <miss.ellenlai@gmail.com>
2014-02-08 12:57:58 -0500

    Generating the Restaurant model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seatyourself2::Application.config.secret_key_base = '519080b09f2ef6e1eddaa086428a3987371536198ff562ff47ff08523778e92c89d2928ac5396a2b8f467a1faf0c0e87f9cd36c06442f67175aad02a104db910'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Seatyourself2::Application.config.session_store :cookie_store, key: '_seatyourself2_session'
