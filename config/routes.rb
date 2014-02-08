cfcead27ef68b7dd6bc5df6aabd7d7aa895ffb47
EllenLai <miss.ellenlai@gmail.com>
2014-02-08 13:12:17 -0500

    Generating the restaurants controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Seatyourself2::Application.routes.draw do
+  resources :restaurants
+
+  root :to => "restaurants#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
