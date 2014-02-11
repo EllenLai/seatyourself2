8262bd483a28fd2cccbc11f2231dc3945abd33f9
EllenLai <miss.ellenlai@gmail.com>
2014-02-11 15:30:23 -0500

    Added New Restaurant Validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,5 @@ class Guest < ActiveRecord::Base
 	has_secure_password
 	has_many :reservations
 	has_many :restaurants, :through => :reservations
+	validates_presence_of :first_name
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reservations
 	has_many :guests, :through => :reservations
+	validates :name, :address, :category, :summary, :presence => true
 end
