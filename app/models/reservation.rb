08b933ba7dbe91aaf34a31c02ef0ebccc6f13571
EllenLai <miss.ellenlai@gmail.com>
2014-02-11 19:29:34 -0500

    Fixed Reservation Timzones

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :restaurant
 	belongs_to :guest
+	validates :number_of_guests, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
 end
